3c425eaee397bf97f514bbe178647c3b98a3db68
Greg King <greg.king@gfs.com>
2024-12-01 00:20:00 -0500

    Day 01

---
+++ src/main/java/king/greg/aoc2024/Day01.java
@@ -0,0 +1,48 @@
+package king.greg.aoc2024;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Day01 {
+
+  private final List<String> lines;
+
+  Day01(final List<String> lines) {
+    this.lines = lines;
+  }
+
+  public long distance()  {
+    var left = new ArrayList<Long>();
+    var right = new ArrayList<Long>();
+    for (var line : lines) {
+      var temp = line.split("\\s+");
+      left.add(Long.parseLong(temp[0]));
+      right.add(Long.parseLong(temp[1]));
+    }
+    left.sort(Long::compareTo);
+    right.sort(Long::compareTo);
+    long distance = 0;
+    for (var i = 0; i < left.size(); i++) {
+      distance += Math.abs(left.get(i) - right.get(i));
+    }
+    return distance;
+  }
+
+  public long similarity()  {
+    var left = new ArrayList<Long>();
+    var right = new HashMap<Long, Long>();
+    for (var line : lines) {
+      var temp = line.split("\\s+");
+      left.add(Long.parseLong(temp[0]));
+      var num = Long.parseLong(temp[1]);
+      right.put(num, right.getOrDefault(num, 0L) + 1);
+    }
+   long similarity =0;
+    for (var num : left) {
+      similarity += num * right.getOrDefault(num, 0L);
+    }
+   return similarity;
+  }
+
+}
---
+++ src/test/java/king/greg/aoc2024/Day01Test.java
@@ -0,0 +1,49 @@
+package king.greg.aoc2024;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Day01Test {
+
+  @Test
+  void testSample1() throws URISyntaxException, IOException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day01/sample1.txt"))
+            .toURI()));
+    final Day01 day01 = new Day01(lines);
+    Assertions.assertThat(day01.distance()).isEqualTo(11);
+  }
+
+  @Test
+  void testSolution1() throws URISyntaxException, IOException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day01/input.txt"))
+            .toURI()));
+    final Day01 day01 = new Day01(lines);
+    Assertions.assertThat(day01.distance()).isEqualTo(3246517);
+  }
+
+  @Test
+  void testSample2() throws URISyntaxException, IOException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day01/sample1.txt"))
+            .toURI()));
+    final Day01 day01 = new Day01(lines);
+    Assertions.assertThat(day01.similarity()).isEqualTo(31);
+  }
+
+  @Test
+  void testSolution2() throws URISyntaxException, IOException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day01/input.txt"))
+            .toURI()));
+    final Day01 day01 = new Day01(lines);
+    Assertions.assertThat(day01.similarity()).isEqualTo(29379307);
+  }
+
+}
