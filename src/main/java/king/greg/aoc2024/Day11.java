a50248c478c0649ca91aab7bdc6adb861a271033
Greg King <greg.king@gfs.com>
2024-12-11 12:50:08 -0500

    Day11 optimizations

--- src/main/java/king/greg/aoc2024/Day11.java
+++ src/main/java/king/greg/aoc2024/Day11.java
@@ -8,7 +8,7 @@ import org.apache.commons.lang3.tuple.Pair;
 public class Day11 {
 
   private final List<String> lines;
-  private final Map<Pair<String, Long>, Long> memo = new ConcurrentSkipListMap<>();
+  private final Map<Pair<Long, Long>, Long> memoCount = new ConcurrentSkipListMap<>();
 
   Day11(final List<String> lines) {
     this.lines = lines;
@@ -18,33 +18,32 @@ public class Day11 {
     final var stones = lines.getFirst().split(" ");
     long count = 0;
     for (String stone : stones) {
-      count += stoneCount(stone, blinks);
+      count += stoneCount(Long.parseLong(stone), blinks);
     }
     return count;
   }
 
-  private long stoneCount(final String stone, final long blinks) {
-    return memo.computeIfAbsent(Pair.of(stone, blinks),
+  private long stoneCount(final long stone, final long blinks) {
+    return memoCount.computeIfAbsent(Pair.of(stone, blinks),
         s -> {
-          var nextStones = blink(stone);
-          if (blinks == 1) {
-            return (long) nextStones.length;
+
+          if (blinks == 0) {
+            return 1L;
           }
-          long calcCount = 0;
-          for (var nextStone : nextStones) {
-            calcCount += stoneCount(nextStone, blinks - 1);
+          int digits = (int) (Math.log10(stone) + 1);
+          long calcCount;
+          if (stone == 0) {
+            calcCount = stoneCount(1, blinks - 1);
+          } else if (digits % 2 == 0) {
+            long halfDigits = digits / 2;
+            long threshold = ((long) Math.pow(10, halfDigits));
+            calcCount = stoneCount(stone / threshold, blinks - 1)
+                + stoneCount(stone % threshold, blinks - 1);
+          } else {
+            calcCount = stoneCount(stone * 2024, blinks - 1);
           }
           return calcCount;
-        });
-  }
-
-  private String[] blink(final String stone) {
-    if ("0".equals(stone)) {
-      return new String[]{"1"};
-    } else if (stone.length() % 2 == 0) {
-      return new String[]{stone.substring(0, stone.length() / 2),
-          stone.substring(stone.length() / 2).replaceFirst("^0+(?!$)", "")};
-    }
-    return new String[]{String.valueOf(Long.parseLong(stone) * 2024)};
+        }
+    );
   }
 }
