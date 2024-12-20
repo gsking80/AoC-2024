94f54401f0f066cf7d9b0a8752e4703e940272b5
Greg King <greg.king@gfs.com>
2024-12-20 01:05:01 -0500

    Day20

---
+++ src/main/java/king/greg/aoc2024/Day20.java
@@ -0,0 +1,114 @@
+package king.greg.aoc2024;
+
+import java.awt.Point;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Day20 {
+
+  private static final List<Point> DIRECTIONS = Arrays.asList(
+      new Point(1, 0),
+      new Point(0, -1),
+      new Point(-1, 0),
+      new Point(0, 1));
+  private final char[][] map;
+  private final Map<Point, Integer> distancesToEnd = new HashMap<>();
+  private Point start;
+  private Point end;
+  private int maxTime;
+
+  Day20(final List<String> lines) {
+    map = new char[lines.size()][lines.getFirst().length()];
+    for (int y = 0; y < lines.size(); y++) {
+      for (int x = 0; x < lines.get(y).length(); x++) {
+        final char space = lines.get(y).charAt(x);
+        switch (space) {
+          case 'S':
+            start = new Point(x, y);
+            map[y][x] = '.';
+            break;
+          case 'E':
+            end = new Point(x, y);
+            map[y][x] = '.';
+            break;
+          default:
+            map[y][x] = space;
+        }
+      }
+    }
+    populateDistanceMap();
+  }
+
+  private void populateDistanceMap() {
+    Point current = new Point(end);
+    int distanceLeft = 0;
+    while (true) {
+      distancesToEnd.put(current, distanceLeft);
+      if (current.equals(start)) {
+        maxTime = distanceLeft;
+        return;
+      }
+      for (final Point direction : DIRECTIONS) {
+        var nextPoint = new Point(current.x + direction.x, current.y + direction.y);
+        if (!distancesToEnd.containsKey(nextPoint) && map[nextPoint.y][nextPoint.x] == '.') {
+          current = nextPoint;
+          distanceLeft++;
+          break;
+        }
+      }
+    }
+  }
+
+  public int calculateShortcuts(final int minimumTimeSaved) {
+    final int[] shortcuts = new int[maxTime];
+    for (final var entry : distancesToEnd.entrySet()) {
+      final Point mapPoint = entry.getKey();
+      final int distanceToEnd = entry.getValue();
+      for (final Point direction1 : DIRECTIONS) {
+        for (final Point direction2 : DIRECTIONS) {
+          final Point testPoint = new Point(mapPoint.x + direction1.x + direction2.x,
+              mapPoint.y + direction1.y + direction2.y);
+          final Integer nextDistanceToEnd = distancesToEnd.get(testPoint);
+          if (nextDistanceToEnd != null) {
+            final int savings = (distanceToEnd - nextDistanceToEnd) - 2;
+            if (savings > 0) {
+              shortcuts[savings]++;
+            }
+          }
+        }
+      }
+    }
+    int shortcutCount = 0;
+    for (int i = minimumTimeSaved; i < shortcuts.length; i++) {
+      shortcutCount += shortcuts[i];
+    }
+    return shortcutCount;
+  }
+
+  public int calculateShortcuts2(final int cheatTime, final int minimumTimeSaved) {
+    final int[] shortcuts = new int[maxTime];
+    for (final var entry : distancesToEnd.entrySet()) {
+      final Point mapPoint = entry.getKey();
+      final int distanceToEnd = entry.getValue();
+      for (final var entry2 : distancesToEnd.entrySet()) {
+        final Point mapPoint2 = entry2.getKey();
+        final int distanceToEnd2 = entry2.getValue();
+        final int shortcutDistance =
+            Math.abs(mapPoint.x - mapPoint2.x) + Math.abs(mapPoint.y - mapPoint2.y);
+        if (shortcutDistance <= cheatTime) {
+          final int timeSaved = (distanceToEnd2 - distanceToEnd) - shortcutDistance;
+          if (timeSaved > 0) {
+            shortcuts[timeSaved]++;
+          }
+        }
+      }
+    }
+    int shortcutCount = 0;
+    for (int i = minimumTimeSaved; i < shortcuts.length; i++) {
+      shortcutCount += shortcuts[i];
+    }
+    return shortcutCount;
+  }
+}
---
+++ src/test/java/king/greg/aoc2024/Day20Test.java
@@ -0,0 +1,48 @@
+package king.greg.aoc2024;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Day20Test {
+
+  @Test
+  void testSample1() throws URISyntaxException, IOException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day20/sample1.txt"))
+            .toURI()));
+    final Day20 day20 = new Day20(lines);
+    Assertions.assertThat(day20.calculateShortcuts(2)).isEqualTo(44);
+  }
+
+  @Test
+  void testSolution1() throws URISyntaxException, IOException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day20/input.txt"))
+            .toURI()));
+    final Day20 day20 = new Day20(lines);
+    Assertions.assertThat(day20.calculateShortcuts(100)).isEqualTo(1448);
+  }
+
+  @Test
+  void testSample2() throws URISyntaxException, IOException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day20/sample1.txt"))
+            .toURI()));
+    final Day20 day20 = new Day20(lines);
+    Assertions.assertThat(day20.calculateShortcuts2(20, 76)).isEqualTo(3);
+  }
+
+  @Test
+  void testSolution2() throws URISyntaxException, IOException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day20/input.txt"))
+            .toURI()));
+    final Day20 day20 = new Day20(lines);
+    Assertions.assertThat(day20.calculateShortcuts2(20, 100)).isEqualTo(1017615);
+  }
+}
